c82632b3de9888c7e3d6c9f103866cfedcf6aca9
Danny <127716079+franco950@users.noreply.github.com>
2025-01-23 21:16:00 +0300

    using js dom to render products instead of html

--- amazon.js
+++ amazon.js
@@ -1,10 +1,11 @@
-const products=[{image:'images/products/countertop-blender-64-oz.jpg',name:'Adults Plain Cotton T-Shirt - 2 Pack',price:1090,rating:{count:87,stars:4.5}},
-    {image:'images/products/countertop-blender-64-oz.jpg',name:'Adults Plain Cotton T-Shirt - 2 Pack',price:1090,rating:{count:87,stars:4.5}},
-    {image:'images/products/countertop-blender-64-oz.jpg',name:'Adults Plain Cotton T-Shirt - 2 Pack',price:1090,rating:{count:87,stars:4.5}},
+const products=[{image:'images/products/athletic-cotton-socks-6-pairs.jpg',name:'Black and Gray Athletic Cotton Socks - 6 Pairs',price:1090,rating:{count:87,stars:4.5}},
+    {image:'images/products/intermediate-composite-basketball.jpg',name:'Intermediate Size Basketball',price:2095,rating:{count:127,stars:4.0}},
+    {image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',name:'Adults Plain Cotton T-Shirt - 2 Pack',price:799,rating:{count:56,stars:4.5}},
     {image:'images/products/countertop-blender-64-oz.jpg',name:'Adults Plain Cotton T-Shirt - 2 Pack',price:1090,rating:{count:87,stars:4.5}},
 ]
+let content='';
 products.forEach((product) => {
-    innerhtml=`<div class="product-container">
+    content+=`<div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
         src=${product.image}>
@@ -23,7 +24,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-price">
-      $${product.price}
+      $${(product.price/100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -53,5 +54,5 @@ products.forEach((product) => {
       </button>
       </div>`
 })
-page=document.body
-page=innerhtml
+document.querySelector('.products-grid').innerHTML+=content
+
